0b08b1f6cbfff3307766101cb6aa24fb1e1050d8
CASTIGATORE <castigatorte2@gmail.com>
2017-01-24 01:03:30 +0100

    Spawner

---
+++ Assets/ETSMB/Script/SpawnBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerBox : MonoBehaviour {
+
+	public Transform[] spawnLocations;
+	public GameObject[] DianogaPrefab;
+	public GameObject[] DianogaClone;
+
+	void Start(){
+		spawnEnemy ();
+	}
+
+
+	void spawnEnemy(){
+		DianogaClone[0] = Instantiate(DianogaPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+}
+
+//copia funzionante spown enemy
---
+++ Assets/ETSMB/Script/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public Transform[] spawnLocations;
+	public GameObject[] DianogaPrefab;
+	public GameObject[] DianogaClone;
+	public bool inTrigger;
+
+	void OnTriggerEnter(Collider other)
+	{
+		inTrigger = true;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		inTrigger = false;
+	}
+
+	void Update()
+	{
+		if (inTrigger)
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				DianogaClone[0] = Instantiate(DianogaPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+				Destroy(this.gameObject);
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		if (inTrigger)
+		{
+			GUI.Box(new Rect(0, 60, 200, 25), "Press E to unlock the door");
+		}
+	}
+}
